5e369717b371b7cfa20ca96d70843a7e72ccd576
firleichjordan <jordanhombing.js@gmail.com>
2024-02-18 07:28:38 +0000

    first commit

---
+++ app/contacts/page.tsx
@@ -0,0 +1,38 @@
+import { CraeteButton } from "@/components/button";
+import ContactTable from "@/components/contact-table";
+import Pagination from "@/components/pagination";
+import Search from "@/components/search";
+import { TableSkeleton } from "@/components/skeleton";
+import { getContactPages } from "@/lib/data";
+import { Suspense } from "react";
+
+const Contacts = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await getContactPages(query);
+
+  return (
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-center gap-1 mb-5">
+        <Search />
+        <CraeteButton />
+      </div>
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <ContactTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
